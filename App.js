c898ec88ce3a827460c97e3ce2bf0c67a3edea3f
matankribus <matanwx135@gmail.com>
2021-09-29 09:23:54 +0300

    Cards in a row

--- App.js
+++ App.js
@@ -7,35 +7,45 @@ const styles = StyleSheet.create({
    flex: 1,
    paddingTop: 22
   },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
+  row: {
+    flexDirection: 'row',
   },
+  cardTitle: {
+  },
+  card: {
+    flexWrap: 'wrap',
+        flexShrink: 1,
+
+  },
+  cardText:{
+    textAlign: 'center',
+  },
+  cardImage: {
+    resizeMode: 'cover'
+  }
 });
 
-function card(title,text,imageSource) {
-    return(
-    <View>
-    <Card>
-      <Card.Title>{title}</Card.Title>
+function card(title,image,text){
+  return(
+    <View style={styles.card}>
+      <Card style={styles.card}>
+      <Card.Title  style={styles.cardTitle }>{title}</Card.Title>
       <Card.Divider/>
-      <Card.Image source ={{uri:imageSource}}>
-            <Card.FeaturedTitle>{text}</Card.FeaturedTitle>
-
-      </Card.Image>
-      <Card.Divider/>
-
-    </Card>
+      <Card.Image style={styles.cardImage} source={{uri:image}}>
+        </Card.Image>
+      <Text style={styles.cardText}>{text}</Text>
+      </Card>
     </View>
   );
 }
 
 
+
 const shower = () =>{
   return(
-    <View >
-    {card("Cat","what a cute cat","https://kerenagam.co.il/wp-content/uploads/2021/06/Thinking-of-getting-a-cat.png")}
+    <View style={styles.row}>
+    {card("Animal","https://cdn.cnn.com/cnnnext/dam/assets/201030094143-stock-rhodesian-ridgeback-super-tease.jpg","what a cute Animal")}
+    {card("an animal that meaos alot and then poops","https://kerenagam.co.il/wp-content/uploads/2021/06/Thinking-of-getting-a-cat.png","another cutie!")}
     </View>
   );
 }
