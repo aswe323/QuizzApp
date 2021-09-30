8c68e2a80c08a1ef7179b0ff4d9833475fc4b121
matankribus <matanwx135@gmail.com>
2021-09-30 11:36:06 +0300

    limited number of characters per title and info in card. began working on grid.

--- App.js
+++ App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
 import { Card, ListItem, Button, Icon } from 'react-native-elements';
 
+const cardTitleMaxLength = 14;
+const cardInfoMaxLength = 20;
+const cardsPerRow = 2;
 const styles = StyleSheet.create({
   container: {
-   flex: 1,
-   paddingTop: 22
+  },
+  grid: {
   },
   row: {
     flexDirection: 'row',
@@ -14,8 +17,6 @@ const styles = StyleSheet.create({
   },
   card: {
     flexWrap: 'wrap',
-        flexShrink: 1,
-
   },
   cardText:{
     textAlign: 'center',
@@ -25,15 +26,42 @@ const styles = StyleSheet.create({
   }
 });
 
+function cardGrid (cards)
+{
+ var returned;
+ var workingRow;
+ var counter = 0;
+  for (const card in cards) {
+    if (Object.hasOwnProperty.call(cards, card)) {
+      if(counter == cardsPerRow){
+        returned = <View style={styles.row}>{workingRow} </View>
+        workingRow = null;
+      }else{
+
+      const currentCard = cards[card];
+      workingRow += currentCard;
+      counter +=1;
+      }
+    }
+  }
+  
+  return(<View stule={styles.grid}> {returned} </View>);
+}
+
 function card(title,image,text){
+  if(title.length < cardTitleMaxLength) title = " ".repeat((cardTitleMaxLength - title.length) / 2) + title + " ".repeat((cardTitleMaxLength - title.length)/2);
+  if(text.length < cardInfoMaxLength) text = " ".repeat((cardInfoMaxLength - text.length)/2) + text + " ".repeat((cardInfoMaxLength - text.length)/2);
+  var workingTitle = title.substring(0,cardTitleMaxLength);
+  var workingInfo = text.substring(0,cardInfoMaxLength);
+
   return(
     <View style={styles.card}>
       <Card style={styles.card}>
-      <Card.Title  style={styles.cardTitle }>{title}</Card.Title>
+      <Card.Title  style={styles.cardTitle }>{workingTitle}</Card.Title>
       <Card.Divider/>
       <Card.Image style={styles.cardImage} source={{uri:image}}>
         </Card.Image>
-      <Text style={styles.cardText}>{text}</Text>
+      <Text style={styles.cardText}>{workingInfo}</Text>
       </Card>
     </View>
   );
@@ -41,11 +69,12 @@ function card(title,image,text){
 
 
 
+
 const shower = () =>{
   return(
     <View style={styles.row}>
-    {card("Animal","https://cdn.cnn.com/cnnnext/dam/assets/201030094143-stock-rhodesian-ridgeback-super-tease.jpg","what a cute Animal")}
-    {card("an animal that meaos alot and then poops","https://kerenagam.co.il/wp-content/uploads/2021/06/Thinking-of-getting-a-cat.png","another cutie!")}
+    {card("animal","https://cdn.cnn.com/cnnnext/dam/assets/201030094143-stock-rhodesian-ridgeback-super-tease.jpg","another cutie! look!")}
+    {card("an animal that","https://kerenagam.co.il/wp-content/uploads/2021/06/Thinking-of-getting-a-cat.png","another cutie! look! this should not be visible")}
     </View>
   );
 }
